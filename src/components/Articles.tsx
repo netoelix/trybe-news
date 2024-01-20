9a852b4db16cdf40a4e4f332f5370f61c9ad15f2
Elix Neto <neto.elix@outlook.com>
2024-01-19 21:19:36 -0300

    Estilizando os artigos

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { formatDistanceToNow, parse } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { ArticleContainer } from '../styles/StyleArticle';
 
 function Articles({ filter }) {
   const allNews = useSelector((state) => state.allNews);
@@ -24,7 +25,7 @@ function Articles({ filter }) {
   }
 
   return (
-    <section>
+    <ArticleContainer>
       {data.length === 0 ? <h1>carregando</h1>
         : data.items.map(({ id, titulo, introducao, data_publicacao, link }, index) => {
           if (index === 0) return null;
@@ -32,22 +33,23 @@ function Articles({ filter }) {
             <article key={ id }>
               <h3>{titulo}</h3>
               <p>{introducao}</p>
-              <p>
-                Publicado
-                {' '}
-                {
+              <div>
+                <p>
+                  Publicado
+                  {' '}
+                  {
             formatDistanceToNow(
               parse(data_publicacao, 'dd/MM/yyyy HH:mm:ss', new Date()),
               { addSuffix: true, locale: ptBR },
             )
 }
-
-              </p>
-              <a href={ link }>Leia a notícia aqui</a>
+                </p>
+                <a href={ link }>Leia a notícia aqui</a>
+              </div>
             </article>
           );
         })}
-    </section>
+    </ArticleContainer>
   );
 }
 
---
+++ src/styles/StyleArticle.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const ArticleContainer = styled.section`
+  background-color: #F1F2F3;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+
+  article {
+    width: 350px;
+    height: 350px;
+    margin: 20px;
+    padding: 20px;
+    box-shadow: 0px 2px 14px rgba(42, 42, 42, 0.24);
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: space-evenly;
+    align-items: center;
+    color: #2A2A2A;
+
+    h3 {
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 600;
+      text-align: justify;
+    }
+
+    p {
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 400;
+      text-align: justify;
+    }
+    div {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      align-content: center;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      a {
+        border-radius: 5px;
+        background: #05D465;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        display: flex;
+        width: 167px;
+        height: 36px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        color: #000;
+        font-size: 14px;
+        text-decoration: none;
+        font-style: normal;
+        font-weight: 700;
+      }
+    }
+  }
+`;
