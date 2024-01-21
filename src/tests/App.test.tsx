0cea54b7f9c1b1e33bb44f66aa5704d8f1b18a15
Elix Neto <neto.elix@outlook.com>
2024-01-21 15:39:39 -0300

    Fix lint

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -30,9 +30,10 @@ const renderWithReduxAndRouter = (component: string | number | boolean | JSX.Ele
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let mockFetchApi;
 
-const setupFetchMockAndRender = (data, component) => {
+const setupFetchMockAndRender = (data: { items: { id: number; tipo: string; titulo: string; introducao: string; data_publicacao: string; produto_id: number; produtos: string; editorias: string; produtos_relacionados: string; destaque: boolean; link: string; }[] | { id: number; tipo: string; titulo: string; introducao: string; data_publicacao: string; produto_id: number; produtos: string; editorias: string; produtos_relacionados: string; destaque: boolean; link: string; }[] | { id: number; tipo: string; titulo: string; introducao: string; data_publicacao: string; produto_id: number; produtos: string; editorias: string; produtos_relacionados: string; destaque: boolean; link: string; }[]; }, component: string | number | boolean | JSX.Element | Iterable<ReactNode> | null | undefined) => {
   const MOCK_RESPONSE = {
     ok: true,
     status: 200,
