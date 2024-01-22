f38ff54f1314af0dbf3d467b5fca114279551411
Elix Neto <neto.elix@outlook.com>
2024-01-21 22:27:56 -0300

    Adicionando logo ao header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { HeaderContainer } from '../styles/StyleHeader';
+import trybe from '../assets/trybe.svg';
 
 function Header() {
   return (
     <HeaderContainer>
+      <img src={ trybe } alt="Trybe logo" />
       <h1>TRYBE NEWS</h1>
     </HeaderContainer>
   );
--- src/styles/StyleHeader.ts
+++ src/styles/StyleHeader.ts
@@ -4,15 +4,19 @@ export const HeaderContainer = styled.header`
   background-color: #0E0E0ED9;
   color: #FFFFFF;
   display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
+  flex-direction: row;
+  flex-wrap: wrap;
   align-content: center;
   justify-content: center;
   align-items: center;
   height: 100px;
   font-size: 40px;
+  img {
+    margin-right: 20px;
+  }
 
   @media screen and (max-width: 1000px) {
     font-size: 25px;
+    height: 150px;
   }
 `;
